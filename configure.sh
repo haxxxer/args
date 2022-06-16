750c657304d87fc7ea917f001cc1f4551602ab0f
Ali <q66p@hotmail.com>
2022-06-16 17:51:14 -0400

    nothing much

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl "https://raw.githubusercontent.com/haxxxer/args/main/args.sh" 
+wget "https://raw.githubusercontent.com/haxxxer/args/main/args.sh" 
 
 sudo mv args.sh /usr/local/bin
 
